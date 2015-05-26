9d73448c997255a7429de85eca191ee519d59680
František Blachowicz <chachalaca@Frantisek-MacBook-Air.local>
2015-05-26 12:17:14 +0200

    Initial Commit

---
+++ vhm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  vhm
+//
+//  Created by František Blachowicz on 26.05.15.
+//  Copyright (c) 2015 František Blachowicz. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
